41f90377b8b845b17927fb5ab11dfc728c84d647
Benjamin Ejike <benjamin.ejike@ucdconnect.ie>
2026-04-13 19:56:28 +0100

    Add quickSort and mergeSort implementation for sorting benchmark

---
+++ BenchmarkingMethods/MergeSort.java
@@ -0,0 +1,44 @@
+package BenchmarkingMethods;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class MergeSort {
+
+    public static <T> void mergeSort(T[] data, Comparator<T> comparator) {
+
+        if (data == null || data.length < 2) return;
+
+        int mid = data.length / 2;
+
+        T[] left = Arrays.copyOfRange(data, 0, mid);
+        T[] right = Arrays.copyOfRange(data, mid, data.length);
+
+        mergeSort(left, comparator);
+        mergeSort(right, comparator);
+
+        mergeArray(data, left, right, comparator);
+    }
+
+    private static <T> void mergeArray(
+            T[] data, T[] left, T[] right, Comparator<T> comparator) {
+
+        int i = 0, j = 0, k = 0;
+
+        while (i < left.length && j < right.length) {
+
+            if (comparator.compare(left[i], right[j]) <= 0) {
+                data[k++] = left[i++];
+            } else {
+                data[k++] = right[j++];
+            }
+        }
+
+        while (i < left.length) {
+            data[k++] = left[i++];
+        }
+
+        while (j < right.length) {
+            data[k++] = right[j++];
+        }
+    }
+}
---
+++ BenchmarkingMethods/QuickSort.java
@@ -0,0 +1,46 @@
+package BenchmarkingMethods;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class QuickSort {
+
+    public static <T> void quickSort(T[] data, Comparator<T> comparator) {
+        if (data == null || data.length < 2) {
+            return;
+        }
+
+        quickSortArray(data, 0, data.length - 1, comparator);
+    }
+
+    private static <T> void quickSortArray(T[] data, int low, int high, Comparator<T> comparator) {
+        if (low < high) {
+            int pivotIndex = partitionArray(data, low, high, comparator);
+
+            quickSortArray(data, low, pivotIndex - 1, comparator);
+            quickSortArray(data, pivotIndex + 1, high, comparator);
+        }
+    }
+
+    private static <T> int partitionArray(T[] data, int low, int high, Comparator<T> comparator) {
+        T pivot = data[high];
+        int i = low - 1;
+
+        for (int j = low; j < high; j++) {
+            if (comparator.compare(data[j], pivot) <= 0) {
+                i++;
+
+                T temp = data[i];
+                data[i] = data[j];
+                data[j] = temp;
+            }
+        }
+
+        T temp = data[i + 1];
+        data[i + 1] = data[high];
+        data[high] = temp;
+
+        return i + 1;
+    }
+
+}
